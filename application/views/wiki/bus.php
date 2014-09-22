e02923887cf7c609d1558b270a65068b6897df44
gmkipladen <gmkipladen@gmail.com>
2014-09-22 15:34:48 +0700

    Change Bus View

--- application/views/wiki/bus.php
+++ application/views/wiki/bus.php
@@ -28,16 +28,13 @@
                     	<!-- Generate Tag Form -->
                     	<div class="route">
                     		<!-- If BusStop is MainPoint then class="main_point" !-->
-                    		<div class="text_point">Test</div>
+                    		<!--<div class="text_point">Test</div>!-->
                     		<a href="#" class="main_point"></a>
                     		<!-- If BusStop is **not MainPoint then class="sub_point" !-->
 							<a href="#" class="sub_point"></a>
 							<a href="#" class="sub_point"></a>
 							<a href="#" class="sub_point"></a>
-							
-							<div class="text_point">Test</div>
 							<a href="#" class="main_point"></a>
-							
 							<a href="#" class="sub_point"></a>
 							<!-- Route Line background must be able to extend responsively !-->
 							<div class="route_line"> </div>
